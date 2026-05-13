e6b8e5a8dd6a434acbb791266a0e84a2402e3422
Sagar <sagarkadam.mk@gmail.com>
2026-05-13 11:13:06 +0000

    fix supabase url and key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wvnfauyimzrofxjdjaxt.supabase.co/rest/v1/";
+const supabaseUrl = "https://wvnfauyimzrofxjdjaxt.supabase.co";
 const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2bmZhdXlpbXpyb2Z4amRqYXh0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODY1MzA3MiwiZXhwIjoyMDk0MjI5MDcyfQ._mcLtjBz3hlk-QsgRa9M3yAQmCMyg-dS5QsrZxFMmRE";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2bmZhdXlpbXpyb2Z4amRqYXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NTMwNzIsImV4cCI6MjA5NDIyOTA3Mn0.-9_yrfwoHVZ9JC5FUJikUDvrJBegno_q0p9YP6tZY_M";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
