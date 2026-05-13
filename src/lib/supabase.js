03ac67f3ab301f7138c2c19996415741ab1335ba
Sagar <sagarkadam.mk@gmail.com>
2026-05-13 11:02:47 +0000

    add supabase credentials

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "YOUR_SUPABASE_URL_HERE";
-const supabaseAnonKey = "YOUR_SUPABASE_ANON_KEY_HERE";
+const supabaseUrl = "https://wvnfauyimzrofxjdjaxt.supabase.co/rest/v1/";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2bmZhdXlpbXpyb2Z4amRqYXh0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODY1MzA3MiwiZXhwIjoyMDk0MjI5MDcyfQ._mcLtjBz3hlk-QsgRa9M3yAQmCMyg-dS5QsrZxFMmRE";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
